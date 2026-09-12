4466f85faf31ba2f12b1c5143d176c2ea6ea93b3
fiyinfholuwa <fiyinfholuwa@gmail.com>
2026-09-12 15:49:29 +0100

    fixied

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ export const dynamic = 'force-dynamic'
 
 import type { Metadata } from "next";
 import { Inter, IBM_Plex_Serif } from "next/font/google";
-import "./app/globals.css";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
 const ibmPlexSerif = IBM_Plex_Serif({
@@ -12,8 +12,8 @@ const ibmPlexSerif = IBM_Plex_Serif({
 })
 
 export const metadata: Metadata = {
-  title: "Horizon",
-  description: "Horizon is a modern banking platform for everyone.",
+  title: "DaraPay",
+  description: "DaraPay is a modern banking platform for everyone.",
   icons: {
     icon: '/icons/logo.svg'
   }
